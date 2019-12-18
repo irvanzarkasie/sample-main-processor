7ac5090dde841f0227abaf400ef2388890ee5ac0
irvanzarkasie <irvanzarkasie@gmail.com>
2019-12-18 16:19:51 +0700

    Initial commit

---
+++ MainExecutionSample.py
@@ -0,0 +1,52 @@
+from pysparkprocessor.wrappers import LoggerWrapper
+import configparser
+import os
+from pysparkprocessor.adapters import PipeExecutor
+from pysparkprocessor.pipelines import CommonHiveReadWritePipeline
+from pysparkprocessor.pipelines import SampleDataFrameMapperPipeline
+
+# Initialize logger
+logger = LoggerWrapper.init_logger(__name__)
+
+# Get current script directory
+main_dir = os.path.dirname(os.path.abspath(__file__))
+logger.debug("Executing main script in " + main_dir + " directory")
+
+# Load config from config file using configparser lib
+logger.debug("Loading configuration from config file")
+config = configparser.ConfigParser()
+config.read(os.path.join(main_dir, "../config/config.ini"))
+logger.debug("Configuration loaded")
+
+# Load pyspark lib
+logger.debug("Calling PysparkWrapper to load pyspark lib")
+from pysparkprocessor.wrappers import PysparkWrapper
+PysparkWrapper.init_pyspark()
+logger.debug("PysparkWrapper successfully loaded pyspark lib")
+
+# Initialize HiveContext for sql read/write purpose
+logger.debug("Calling PysparkWrapper to initialize hive context")
+PysparkWrapper.init_pyspark()
+hive_context = PysparkWrapper.get_hivecontext("SampleMainPipeline")
+logger.debug("Hive context initialized")
+
+# Execute pipeline operations for data processing
+logger.debug("Executing hive pipelines to process data")
+result = PipeExecutor.get_execution_result(
+  PipeExecutor.execute(
+    *[
+      CommonHiveReadWritePipeline.read_from_table, 
+      SampleDataFrameMapperPipeline.map_read_output_to_write_input, 
+      CommonHiveReadWritePipeline.insert_to_table
+    ], 
+    **{
+        "input_table":"default.test20", 
+        "output_table":"default.test30", 
+        "input":None, 
+        "output":None,
+        "sqlcontext": hive_context
+    }
+  )
+)
+logger.debug("Finish executing pipeline")
+result["output"].show()
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
+setuptools.setup(
+    name="sample-processor",
+    version="0.0.1",
+    author="expecc",
+    author_email="expecc@expecc.com",
+    description="Sample processor",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=load_requirements("requirements.txt")
+)
